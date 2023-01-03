142b4bd61436e9b3f515387ca6d7f7a7fe902d48
qhc004 <qhc004@qq.com>
2023-01-03 16:04:38 +0800

    add icon component

---
+++ base/web/src/icon/index.ts
@@ -0,0 +1,53 @@
+import EovElement from "../eov"
+//EovElement.prefix = 'new'
+
+import './../assets/styles/button.scss'
+import { addTemplateElement as template } from "../utils"
+
+// 参考：https://ionic.io/ionicons
+export default class Icon extends EovElement{
+
+  constructor(opts: any) {
+    super(opts)
+  }
+
+  connectedCallback() {
+    let shadow = this.attachShadow({mode: 'open'})
+    const html = `
+      <style scoped>
+      :host {
+        display: inline-block;
+        cursor: pointer;
+        padding: 2px 8px;
+        border: 1px solid #999;
+        border-radius: 3px;
+      }
+      :host([aria-disabled="true"]) {
+        cursor: not-allowed;
+      }
+      </style>
+      <span class="component-container">
+        <button id="proxy-button" tabindex="-1" style="display:none;"></button>
+        <slot part="default"></slot>
+        <slot part="icon" name="icon"></slot>
+      </span>
+    `
+
+    let tplEl = template(html, {}, shadow)
+    let content = tplEl.content.cloneNode(true)
+    shadow.append(content)
+
+    this.#setDefaultAttrs()
+  }
+
+
+  // 设置默认属性
+  #setDefaultAttrs() {
+    if (!this.hasAttribute('role')) {
+      this.setAttribute('role', 'icon')
+    }
+    if (!this.hasAttribute('tabindex')) {
+      this.setAttribute('tabindex', '0')
+    }
+  }
+}
